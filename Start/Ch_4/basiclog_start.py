e6a98543588e9cf67b8d466d7d8ed48ce49c696f
JooyoungYoo01 <139180373+JooyoungYoo01@users.noreply.github.com>
2023-09-11 06:36:18 +0000

    Finish

--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,29 @@
 # demonstrate the logging api in Python
+import logging
 
 # TODO: use the built-in logging module
 
 
-# TODO: Use basicConfig to configure logging
+# TODO: Use basicConfig to configure logging # change logging level!
+logging.basicConfig(level=logging.DEBUG,
+                    filemode="w",
+                    filename="output.log")
 
-# TODO: Try out each of the log levels
+# Try out each of the log levels
+logging.debug("This is a debug-level log message")
+logging.info("This is an info-level log message")
+logging.warning("This is a warning-level message")
+logging.error("This is an error-level message")
+logging.critical("This is a critical-level message")
 
-# TODO: Output formatted strings to the log
+# Output formatted string to the log
+logging.info("Here's a {} variable and an int: {}".format("string", 10))
 
+##################################################
+# DEBUG:root:This is a debug-level log message
+# INFO:root:This is an info-level log message
+# WARNING:root:This is a warning-level message
+# ERROR:root:This is an error-level message
+# CRITICAL:root:This is a critical-level message
+# INFO:root:Here's a string variable and an int: 10
+##################################################
--- Start/Ch_4/customlog_start.py
+++ Start/Ch_4/customlog_start.py
@@ -1,3 +1,22 @@
+# # Demonstrate how to customize logging output
+
+# import logging
+
+# # TODO: add another function to log from
+
+
+# # set the output file and debug level, and
+# # TODO: use a custom formatting specification
+# logging.basicConfig(filename="output1.log",
+#                     level=logging.DEBUG)
+
+# logging.info("This is an info-level log message")
+# logging.warning("This is a warning-level message")
+##########################################################
+# INFO:root:This is an info-level log message
+# WARNING:root:This is a warning-level message
+##########################################################
+
 # Demonstrate how to customize logging output
 
 import logging
@@ -5,11 +24,26 @@ import logging
 # TODO: add another function to log from
 
 
+def anotherFunction():
+    logging.debug("This is a debug-level log message", extra=extData)
+
+
 # set the output file and debug level, and
 # TODO: use a custom formatting specification
-logging.basicConfig(filename="output.log",
-                    level=logging.DEBUG)
+fmtStr = "User: %(user)s %(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s"
+dateStr = "%m/%d/%Y %I:%M:%S %p"
+extData = {'user': 'joem@example.com'}
 
-logging.info("This is an info-level log message")
-logging.warning("This is a warning-level message")
+logging.basicConfig(filename="output2.log",
+                    level=logging.DEBUG,
+                    format=fmtStr,
+                    datefmt=dateStr)
 
+logging.info("This is an info-level log message", extra=extData)
+logging.warning("This is a warning-level message", extra=extData)
+anotherFunction()
+##########################################################
+# User: joem@example.com 09/11/2023 06:11:48 AM: INFO: <module> Line:43 This is an info-level log message
+# User: joem@example.com 09/11/2023 06:11:48 AM: WARNING: <module> Line:44 This is a warning-level message
+# User: joem@example.com 09/11/2023 06:11:48 AM: DEBUG: anotherFunction Line:28 This is a debug-level log message
+##########################################################
